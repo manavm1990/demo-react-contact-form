123f6a5cff23cacd0406b596a2014a43286b43c9
Manav Misra <manavm@visionwebsoft.com>
2023-03-14 05:40:55 -0500

    refactor: :fire: remove unused test ✅ file

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import { expect, test } from "vitest";
-
-test("renders correctly", () => {
-  render(<App />);
-  expect(screen.getByText("Hello world!")).toBeInTheDocument();
-});
