b5b16b6d649fb8c70c780d8a53279889ad714cc1
Manav Misra <manavm@visionwebsoft.com>
2023-03-14 06:54:11 -0500

    build: :rocket: add dumb, stupid 'base' option to vite.config
    It's for GH Pages 💩.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/demo-react-contact-form/",
   plugins: [react()],
   resolve: {
     alias: {
