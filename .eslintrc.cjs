7e9aa5e4e092a294c8eaea77f4ba8fa9b0b99fdd
Manav Misra <manavm@visionwebsoft.com>
2023-03-13 23:38:41 -0500

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    'plugin:testing-library/react',
+    "plugin:jest-dom/recommended",
+    "standard",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "use-encapsulation", "testing-library", "jest-dom"],
+  rules: { "use-encapsulation/prefer-custom-hooks": 1 },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
